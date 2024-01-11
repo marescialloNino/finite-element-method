aceab98aa05436d636c101d657fb52dbd8dcea0e
RichardNixon <nicola.schiavo@studenti.unipd.it>
2024-01-11 23:47:35 +0100

    add error logic and plots

--- create_sparsity_pattern.py
+++ create_sparsity_pattern.py
@@ -5,16 +5,25 @@ import matplotlib.pyplot as plt
 import ilupp
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.tri as mtri
+from scipy.interpolate import LinearNDInterpolator
+import pyamg
 
 # File paths for the uploaded files
 topol_file = './meshes/mesh0/mesh0.topol'
 coord_file = './meshes/mesh0/mesh0.coord'
 bound_file = './meshes/mesh0/mesh0.bound'
+trace_file = './meshes/mesh0/mesh0.trace'
+track_file = './meshes/mesh0/mesh0.track'
+ref_grid_file = './meshes/refgrid.csv'
 
 # Load the data from the files
-topol = pd.read_csv(topol_file, sep="\s+", header=None).to_numpy()
+topol = pd.read_csv(topol_file, sep="\s+", header=None).to_numpy() 
 coord = pd.read_csv(coord_file, sep="\s+", header=None).to_numpy()
 bound = pd.read_csv(bound_file, sep="\s+", header=None).to_numpy()
+trace = pd.read_csv(trace_file, sep="\s+", header=None).to_numpy()
+track = pd.read_csv(track_file, sep="\s+", header=None).to_numpy()
+ref_grid = pd.read_csv(ref_grid_file, delim_whitespace=True, header=None).to_numpy()
+
 
 n = len(coord)
 H = sp.csr_matrix((n,n))
@@ -101,23 +110,35 @@ plt.show()
 
 
 
-def enforce_dirichlet_conditions(K1,K2, bound, u0):
+def enforce_dirichlet_conditions(K1,K2, bound, u0, current_time):
     nBound = bound.shape[0]
     H = K1.copy()
     rhs = K2 @ u0
-
+    tmax=10
     for i in range(nBound):
+        node_index = int(bound[i, 0]) - 1
+        boundary_type = bound[i, 1]
+
+        # Determine the boundary value based on the type and current time
+        if boundary_type == 0: 
+            boundary_value = 0  # Constant zero
+        elif boundary_type == 1:
+            if current_time <= tmax / 2:
+                boundary_value = 2 * current_time / tmax  # Linearly increasing
+            else:
+                boundary_value = 1  # Constant at 1
+
         # Set all the Dirichlet rows in H to zero
-        H[int(bound[i, 0]), :] = 0
+        H[node_index, :] = 0
         # Update the right-hand side for the Dirichlet boundary
-        column_to_subtract = H[:, int(bound[i, 0])].toarray().flatten() * bound[i, 1]
+        column_to_subtract = H[:, node_index].toarray().flatten() * boundary_value
         rhs -= column_to_subtract
         # set Dirichlet columns in H to zero
-        H[:, int(bound[i, 0])] = 0
+        H[:, node_index] = 0
         # Set diagonal term to 1 for Dirichlet nodes in H
-        H[int(bound[i, 0]), int(bound[i, 0])] = 1
+        H[node_index, node_index] = 1
         # Set rhs to dirichlet condition value in the bound file
-        rhs[int(bound[i, 0])] = bound[i, 1]      
+        rhs[node_index] = boundary_value      
 
     return H, rhs
 
@@ -126,15 +147,34 @@ def solve_system(K1, K2, bound, u0, tmax, deltat):
     u = u0.copy()
     U=[]
     flags=[]
-    while t <= tmax  :
-        H, rhs = enforce_dirichlet_conditions(K1, K2, bound, u)
+    while t <= tmax :
+        H, rhs = enforce_dirichlet_conditions(K1, K2, bound, u, t)
         
         M = ilupp.IChol0Preconditioner(H)
-        u,flag=sp.linalg.cg(H,rhs,u0,tol=1e-08,maxiter=100,M=M)
+        
+        u,flag=sp.linalg.cg(H,rhs,u0,tol=1e-04,maxiter=100,M=M)
+
         U.append(u)
         flags.append(flag)
         t += deltat
     return U,flags
+
+def compute_error(u, coord, ref_grid, f):
+
+    x_coords = coord[:, 0]
+    y_coords = coord[:, 1]
+    # Interpolate the reference solution onto your mesh
+    interp = LinearNDInterpolator((ref_grid[:,0], ref_grid[:,1]), ref_grid[:,2])
+
+    ref_solution_interpolated = interp(x_coords, y_coords)
+    eps_squared = 0
+    for i in range(len(u)):
+        eps_squared += ((u[i] - ref_solution_interpolated[i])**2)*f[i]
+    # Compute the error
+    eps = np.sqrt(eps_squared)
+    
+    return eps
+
 
 delta_t = 0.02
 theta= 0.5
@@ -143,9 +183,37 @@ K2 = (M/delta_t) - ((1-theta) * H)
 u0 = np.zeros(K1.shape[0])
 U,iter = solve_system(K1,K2,bound,u0,10,delta_t)
 
-print(len(U),iter)
+epsilon = compute_error(U[-1],coord,ref_grid,f)
 
+print(epsilon)
 
+times = np.arange(0, 10.02, 0.02)
+track_solutions = []
+for i in track:
+    
+    node_index = i
+    sol = []
+    for solution in U:
+        sol.append(solution[node_index])
+        # Create the plot
+    track_solutions.append(sol)    
+
+plt.plot(times, track_solutions[0], marker='o', linestyle='-')
+plt.plot(times, track_solutions[1], marker='o', linestyle='-')
+plt.plot(times, track_solutions[2], marker='o', linestyle='-')
+# Add titles and labels
+plt.title('Node Values Over Time')
+plt.xlabel('Time')
+plt.ylabel('u')
+
+# Optionally, you can add a grid for better readability
+plt.grid(True)
+
+# Show the plot
+plt.show()
+
+""" error_last_time_step = compute_error(U[-1], x_coords, y_coords, xRef, yRef, uRef)
+print("Error at the last time step:", error_last_time_step) """
 
 # Example Data: Replace these with your actual data
  # Y-coordinates of nodes
